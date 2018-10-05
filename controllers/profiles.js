81c64e7cdcfe565c404619b629391453cf898a87
Jonathan Linch <junglecat@Jonathans-MacBook-Pro.local>
2018-10-05 13:11:31 -0700

    most of the routing for voting is in place, still need redirect after final ballot

--- controllers/profiles.js
+++ controllers/profiles.js
@@ -17,16 +17,94 @@ router.get('/', loggedIn, function(req, res){
 	}).catch(function(err){
 		console.log("Trouble getting db info")
 	})
-	
 });
 
-//define PUT routes
-router.put('/startvote/:id', loggedIn, function(req, res){
-	db.nov2018.update(req.body, {
-		where: {id: req.params.id}
+router.get('/measure-i940', loggedIn, function(req, res){
+	res.render('measures/i-940')
+})
+
+router.get('/measure-i1631', loggedIn, function(req, res){
+	res.render('measures/i-1631')
+})
+
+router.get('/measure-i1634', loggedIn, function(req, res){
+	res.render('measures/i-1634')
+})
+
+router.get('/measure-i1639', loggedIn, function(req, res){
+	res.render('measures/i-1639')
+})
+
+router.get('/measure-av19', loggedIn, function(req, res){
+	res.render('measures/av-19')
+})
+
+//Define PUT routes
+router.put('/measure-i940', loggedIn, function(req, res){
+	console.log("the put body is", req.body)
+	db.nov2018.update({
+		i940: req.body.group1
+	}, {
+		where: {userId: req.user.id}
 	}).then(function(update){
-		console.log("Successful put")
-		res.redirect("profile/index", { voterInfo: update})
+		console.log("we are getting to the the then in the put")
+		res.send("Success!")
+	}).catch(function(err){
+		console.log('WELL THAT DID NOT WORK!')
+	});
+})
+
+router.put('/measure-i1631', loggedIn, function(req, res){
+	console.log("the put body is", req.body)
+	db.nov2018.update({
+		i1631: req.body.group1
+	}, {
+		where: {userId: req.user.id}
+	}).then(function(update){
+		console.log("we are getting to the the then in the put")
+		res.send("Success!")
+	}).catch(function(err){
+		console.log('WELL THAT DID NOT WORK!')
+	});
+})
+
+router.put('/measure-i1634', loggedIn, function(req, res){
+	console.log("the put body is", req.body)
+	db.nov2018.update({
+		i1634: req.body.group1
+	}, {
+		where: {userId: req.user.id}
+	}).then(function(update){
+		console.log("we are getting to the the then in the put")
+		res.send("Success!")
+	}).catch(function(err){
+		console.log('WELL THAT DID NOT WORK!')
+	});
+})
+
+router.put('/measure-i1639', loggedIn, function(req, res){
+	console.log("the put body is", req.body)
+	db.nov2018.update({
+		i1639: req.body.group1
+	}, {
+		where: {userId: req.user.id}
+	}).then(function(update){
+		console.log("we are getting to the the then in the put")
+		res.send("Success!")
+	}).catch(function(err){
+		console.log('WELL THAT DID NOT WORK!')
+	});
+})
+
+router.put('/measure-av19', loggedIn, function(req, res){
+	console.log("the put body is", req.body)
+	db.nov2018.update({
+		av19: req.body.group1
+	}, {
+		where: {userId: req.user.id}
+	}).then(function(update){
+		console.log("we are getting to the the then in the put")
+		res.send("Success!")
 	}).catch(function(err){
 		console.log('WELL THAT DID NOT WORK!')
 	});
@@ -37,7 +115,7 @@ router.post('/startvote', loggedIn, function(req, res){
 	console.log("THE POST IS SENDING: ", req.body)
 	req.body.userId = req.user.id
 	db.nov2018.create(req.body).then(function(newest){
-		res.redirect("measures/i-940")
+		res.redirect("measure-i940")
 		console.log('SUCCESS!')
 	}).catch(function(err){
 		console.log(err);
--- public/js/ajaxCalls.js
+++ public/js/ajaxCalls.js
@@ -1,17 +1,91 @@
 $(document).ready(function(){
 	console.log('Hi from ajax')
 
-	$('#add-vote').submit(function(e){
+	$('#add-940').submit(function(e){
 		e.preventDefault();
 
-		console.log('This is just before the ajax call')
+        console.log('This is just before the ajax call')
+        var url = $(this).attr('action')
+        console.log("URL:",url)
 		$.ajax({
 			url:$(this).attr('action'),
 			method: 'PUT',
 			data: $(this).serialize()
 		}).done(function(res){
-			console.log('success', res)
-			//window.location = '/profile/pastsight'
+            console.log('success', res)
+            window.location.href = "/profile/measure-i1631"
+		}).fail(function(err){
+			console.log('error', err)
+		})
+    })
+    
+    $('#add-1631').submit(function(e){
+		e.preventDefault();
+
+        console.log('This is just before the ajax call')
+        var url = $(this).attr('action')
+        console.log("URL:",url)
+		$.ajax({
+			url:$(this).attr('action'),
+			method: 'PUT',
+			data: $(this).serialize()
+		}).done(function(res){
+            console.log('success', res)
+            window.location.href = "/profile/measure-i1634"
+		}).fail(function(err){
+			console.log('error', err)
+		})
+    })
+    
+    $('#add-1634').submit(function(e){
+		e.preventDefault();
+
+        console.log('This is just before the ajax call')
+        var url = $(this).attr('action')
+        console.log("URL:",url)
+		$.ajax({
+			url:$(this).attr('action'),
+			method: 'PUT',
+			data: $(this).serialize()
+		}).done(function(res){
+            console.log('success', res)
+            window.location.href = "/profile/measure-i1639"
+		}).fail(function(err){
+			console.log('error', err)
+		})
+    })
+    
+    $('#add-1639').submit(function(e){
+		e.preventDefault();
+
+        console.log('This is just before the ajax call')
+        var url = $(this).attr('action')
+        console.log("URL:",url)
+		$.ajax({
+			url:$(this).attr('action'),
+			method: 'PUT',
+			data: $(this).serialize()
+		}).done(function(res){
+            console.log('success', res)
+            window.location.href = "/profile/measure-av19"
+		}).fail(function(err){
+			console.log('error', err)
+		})
+    })
+    
+    $('#add-av19').submit(function(e){
+		e.preventDefault();
+
+        console.log('This is just before the ajax call')
+        var url = $(this).attr('action')
+        console.log("URL:",url)
+		$.ajax({
+			url:$(this).attr('action'),
+			method: 'PUT',
+			data: $(this).serialize()
+		}).done(function(res){
+            console.log('success', res)
+            window.location.href = "/profile/"
 		}).fail(function(err){
 			console.log('error', err)
 		})
